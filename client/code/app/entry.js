ecdc39ae0dff4fd79b6df5bbc7c72b7d2a298d28
Ignacio Heredia <ignacio.nh@gmail.com>
2015-09-21 17:24:07 -0300

    Epa client-script changes

--- client/code/app/entry.js
+++ client/code/app/entry.js
@@ -5,11 +5,11 @@
 window.ss = require('socketstream');
 
 ss.server.on('disconnect', function(){
-  console.log('Connection down');
+    console.log('Connection down');
 });
 
 ss.server.on('reconnect', function(){
-  console.log('Connection back up');
+    console.log('Connection back up');
 });
 
 var config = [
@@ -28,41 +28,41 @@ var config = [
 ];
 
 var getGeo = function(cb){
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-    var result = {"latitude" : position.coords.latitude, "longitude": position.coords.longitude}
-      cb(result)
-    })
-  }
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var result = {"latitude" : position.coords.latitude, "longitude": position.coords.longitude}
+            cb(result)
+        })
+    }
 };
 
 ss.server.on('ready', function(){
 
-  var pathname = window.location.pathname;
-  // Wait for the DOM to finish loading
-  jQuery(function(){
+    var pathname = window.location.pathname;
+    // Wait for the DOM to finish loading
+    jQuery(function(){
 
-    for (var i = 0;i<config.length;i++){
+        for (var i = 0;i<config.length;i++){
 
-      if (pathname == config[i].path){
+            if (pathname == config[i].path){
 
-        getGeo(function(result){
-          if (result)
-            console.log(result);
-            ss.rpc('bicis.calDistance', result);
-            // setTimeout(function() {
-            //   ss.rpc('bicis.miEstacion')
-            // }, 1000);
-        });
+                getGeo(function(result){
+                    if (result)
+                        console.log(result);
+                    ss.rpc('bicis.calDistance', result);
+                    // setTimeout(function() {
+                    //   ss.rpc('bicis.miEstacion')
+                    // }, 1000);
+                });
 
-        for (var n=0;n<config[i]["widgets"].length;n++){
-          require(config[i]["widgets"][n]);
+                for (var n=0;n<config[i]["widgets"].length;n++){
+                    require(config[i]["widgets"][n]);
+                }
+
+            }
+
         }
-      
-      }
 
-    }
-
-  });
+    });
 
 });
--- config.js
+++ config.js
@@ -3,20 +3,20 @@ module.exports = {
 		"name": "dashboardcivico",
 		"ip": process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
 		"port": process.env.OPENSHIFT_NODEJS_PORT || 8000
-	}, 
+	},
 	"redis": {
 		"host": process.env.OPENSHIFT_REDIS_HOST || "127.0.0.1", // 10.200.183.128
 		"port": process.env.OPENSHIFT_REDIS_PORT || 6379,
 		"auth": process.env.REDIS_PASSWORD || "",
 		"db": process.env.OPENSHIFT_REDIS_DB_NAME || 0
 	}, "routes" : [
-    {
-      "name": "main",
-      "path": "/",
-      "widgets": ["epa"],
-      "view" : "app.html",
-      "css": ["libs/reset.css","app.css"],
-      "code" : ["libs/jquery.min.js", "libs/odometer.min.js", "libs/bootstrap.min.js", "app"]
-    }
-  ]
+		{
+			"name": "main",
+			"path": "/",
+			"widgets": ["epa"],
+			"view" : "app.html",
+			"css": ["libs/reset.css","app.css"],
+			"code" : ["libs/jquery.min.js", "libs/odometer.min.js", "libs/bootstrap.min.js", "app"]
+		}
+	]
 };
